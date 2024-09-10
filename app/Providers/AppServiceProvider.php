b91aa8e7e19944a5bec5a0fc8c9c43bcd774aa81
NIdial.Abd <kol.eljra7.90@gmail.com>
2024-09-10 23:46:44 +0400

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,6 +28,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->app->bind('path.public', function() {
+            return base_path('public_html');
+        });
         // Set default string length for older MySQL versions
         Schema::defaultStringLength(191);
 
