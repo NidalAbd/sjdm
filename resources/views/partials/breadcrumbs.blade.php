3efe664e090b61df92a15e52556809b6a742a0d4
USER <koleljra7@gmail.com>
2025-05-20 01:52:14 +0400

    ss

--- resources/views/partials/breadcrumbs.blade.php
+++ resources/views/partials/breadcrumbs.blade.php
@@ -30,21 +30,5 @@
         @endforeach
     </ol>
 </nav>
-<script type="application/ld+json">
-    {
-        "@context": "https://schema.org",
-        "@type": "BreadcrumbList",
-        "itemListElement": [
-    @foreach($breadcrumbs as $index => $breadcrumb)
-        {
-            "@type": "ListItem",
-            "position": {{ $index + 1 }},
-            "name": "{{ $breadcrumb['title'] }}",
-            "item": "{{ $breadcrumb['url'] }}"
-        }@if(!$loop->last),@endif
-    @endforeach
-    ]
-}
-</script>
 
 @include('partials.alert')
