423c202993bd968e6d08ddfa9de46d42830b82d9
USER <koleljra7@gmail.com>
2025-06-26 21:51:57 +0400

    ss

--- routes/api.php
+++ routes/api.php
@@ -31,6 +31,6 @@ Route::get('/orders/{order}/cancel', [OrderController::class, 'checkCancel'])->n
 Route::get('/{order}/check-cancel', [OrderController::class, 'checkCancel'])->name('checkCancel');
 Route::post('/{order}/cancel', [OrderController::class, 'cancel'])->name('cancel');
 Route::get('/{order}/check-refill', [OrderController::class, 'checkRefill'])->name('checkRefill');
-Route::post('/{order}/refill', [OrderController::class, 'refill'])->name('refill');
+Route::post('/{order}/refill', [OrderController::class, 'refill'])->name('orders.refill');
 
 
